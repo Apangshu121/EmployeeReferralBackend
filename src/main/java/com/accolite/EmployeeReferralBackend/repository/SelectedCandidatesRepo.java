92e226a86eaca096bc4e122151dd05a0ed68c870
uddagirilahari <152380472+uddagirilahari@users.noreply.github.com>
2024-01-11 11:09:33 +0530

    Delete src/main/java/com/accolite/EmployeeReferralBackend/repository directory

--- src/main/java/com/accolite/EmployeeReferralBackend/repository/SelectedCandidatesRepo.java
+++
@@ -1,7 +0,0 @@
-package com.accolite.EmployeeReferralBackend.repository;
-
-import com.accolite.EmployeeReferralBackend.models.SelectedCandidates;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SelectedCandidatesRepo extends JpaRepository<SelectedCandidates, Integer> {
-}
